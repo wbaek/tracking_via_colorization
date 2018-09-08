e1b56626a70fe53dc431fd45259f56557afe0482
clint.b <clint.b@kakaobrain.com>
2018-09-09 00:05:40 +0900

    fix build failure

--- tracking_via_colorization/utils/__init__.py
+++ tracking_via_colorization/utils/__init__.py
@@ -7,7 +7,3 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from .elapsed import Elapsed
-from .utils import grid_images
-from .io import Reader, Writer
-from .devices import Devices
-from .multiple import build_learning_rate, average_gradients
